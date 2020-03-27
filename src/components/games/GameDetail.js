8c8b7233ef166e37eaf3a9d0f522dc8c0bda3535
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-03-27 12:33:42 +0100

    removed some old code and notes

--- src/components/games/GameDetail.js
+++ src/components/games/GameDetail.js
@@ -28,8 +28,6 @@ function GameDetail() {
     return <Spinner animation="grow" variant="light" className="spinner" />;
   }
 
- 
- 
   return (
     <div className="detailBox">
     <h1 className="mainHeader">{detail.name}</h1>
@@ -50,84 +48,8 @@ function GameDetail() {
       </Row>
     </div>
   );
-
-  
 }
 
 
 export default GameDetail
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { BASE_URL } from "../../constants/API";
-
-function GameDetail() {
-  const [details, setDetails] = useState({});
-
-  // get the id from the url
-  const { id } = useParams();
-  console.log("id", id);
-
-  const url = BASE_URL + "/game/" + id;
-
-  console.log(url);
-
-  useEffect(function() {
-    fetch(url)
-      .then(response => response.json())
-      .then(result => {
-        console.log(result);
-        setDetails(result);
-      })
-      .catch(error => console.log(error));
-  }, []);
-
-  return (
-    <div>
-      <h2>{details.name}</h2>
-      <p>Released: {details.released}</p>
-      <p>Rating: {details.rating}</p>
-    </div>
-  );
-}
-
-export default GameDetail;
-
-*/
